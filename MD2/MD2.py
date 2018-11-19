b14474a7dce225eacee75f6726ec45248bd5fe47
Artūrs Stafanovičs <arturs.stafanovics@gmail.com>
2018-11-19 20:50:46 +0200

    Comments

--- MD2/MD2.py
+++ MD2/MD2.py
@@ -20,37 +20,64 @@ class Dice:
     ])
 
     def __init__(self, load=False):
+        """
+        :param load: should model weights be loaded from file 'md2'
+        """
         if load:
             self.model.load_weights('md2')
         else:
-            data, answers = self._load_data([Dice._read_vectors("data/dice_0{}.dat".format(i)) for i in range(9)])
+            # load only 0-8 files, last(9) used for benchmarking
+            data, answers = self.load_data([Dice.read_vectors("data/dice_0{}.dat".format(i)) for i in range(9)])
             self.model.compile(optimizer='adam', loss='categorical_crossentropy')
             for i in range(60):
                 self.model.fit(data, answers, epochs=1, batch_size=1028, verbose=1)
                 self.model.save_weights('md2')
 
     def benchmark_model(self, filename):
-        data, answers = self._load_data([Dice._read_vectors(filename)])
+        """
+        :param filename: file name to load test samples from
+        :return: number of prediction errors
+        """
+        data, answers = self.load_data([Dice.read_vectors(filename)])
         errors = 0
         for sample_data, sample_answer in zip(data, answers):
             result = np.argmax(self.model.predict(np.array([sample_data])))
             if np.argmax(sample_answer) != result:
-                plt.imshow(sample_data.reshape(20, 20))
-                plt.show()
                 errors += 1
-        print(errors)
-        return (1 - errors / len(data)) * 100
+        print("{} erros from {} test samples, {}% precision".format(errors, len(data), (1 - errors / len(data)) * 100))
+        return errors
+
+    def predict(self, image):
+        """
+        :param image: 20 by 20 by 1 numpy array
+        :return: dice value sum
+        """
+        plt.imshow(image.reshape(20, 20))
+        plt.show()
+        return np.argmax(self.model.predict(np.array([image])))
 
     @staticmethod
-    def _load_data(vectors):
+    def load_data(vectors):
+        """
+        :param vectors: x by 401 numpy array
+        :return: ready for training images and labels
+        """
         dice = np.vstack(tuple(vectors))
         return dice[:, 1:].reshape(len(dice), 20, 20, 1) / 255, to_categorical(dice[:, 0])
 
     @staticmethod
-    def _read_vectors(filename):
+    def read_vectors(filename):
+        """
+        :param filename: path to .dat file containing samples
+        :return: raw numpy array containing samples
+        """
         return np.fromfile(filename, dtype=np.uint8).reshape(-1, 401)
 
 
 if __name__ == "__main__":
     dice = Dice(True)
-    print(dice.benchmark_model('data/dice_00.dat'))
+    dice.benchmark_model('data/dice_09.dat')
+    image = Dice.load_data(Dice.read_vectors('data/dice_09.dat'))[0][0]
+    image2 = Dice.load_data(Dice.read_vectors('data/dice_09.dat'))[0][5]
+    print(dice.predict(image))
+    print(dice.predict(image2))
