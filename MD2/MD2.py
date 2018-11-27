5311e7d2b5616c8da78f275c7d2e27e2f66d30de
Artūrs Stafanovičs <arturs.stafanovics@gmail.com>
2018-11-27 19:49:45 +0200

    Clearer interface

--- MD2/MD2.py
+++ MD2/MD2.py
@@ -27,7 +27,7 @@ class Dice:
             self.model.load_weights('md2')
         else:
             # load only 0-8 files, last(9) used for benchmarking
-            data, answers = self.load_data([Dice.read_vectors("data/dice_0{}.dat".format(i)) for i in range(9)])
+            data, answers = self.load_data([f'data/dice_0{i}.dat' for i in range(9)])
             self.model.compile(optimizer='adam', loss='categorical_crossentropy')
             for i in range(60):
                 self.model.fit(data, answers, epochs=1, batch_size=1028, verbose=1)
@@ -38,13 +38,14 @@ class Dice:
         :param filename: file name to load test samples from
         :return: number of prediction errors
         """
-        data, answers = self.load_data([Dice.read_vectors(filename)])
+        data, answers = self.load_data(filename)
         errors = 0
         for sample_data, sample_answer in zip(data, answers):
             result = np.argmax(self.model.predict(np.array([sample_data])))
             if np.argmax(sample_answer) != result:
                 errors += 1
-        print("{} erros from {} test samples, {}% precision".format(errors, len(data), (1 - errors / len(data)) * 100))
+
+        print(f'{errors} errors from {len(data)} test samples, {1 - errors / len(data) * 100} % precision')
         return errors
 
     def predict(self, image):
@@ -57,27 +58,30 @@ class Dice:
         return np.argmax(self.model.predict(np.array([image])))
 
     @staticmethod
-    def load_data(vectors):
+    def load_data(source):
         """
-        :param vectors: x by 401 numpy arrays
-        :return: ready for training images and labels
+        :param source: file or list of file names containing dice samples
+        :return: ready for training (images, labels) in shape - len(samples) by 20 by 20 by 1
         """
-        dice = np.vstack(tuple(vectors))
-        return dice[:, 1:].reshape(len(dice), 20, 20, 1) / 255, to_categorical(dice[:, 0])
+        if isinstance(source, (list,)):
+            vectors = [Dice._read_vectors(file) for file in source]
+        elif isinstance(source, str):
+            vectors = Dice._read_vectors(source)
+        else:
+            raise ValueError('expecting string or list of strings')
+
+        stacked_vector = np.vstack(tuple(vectors))
+        return stacked_vector[:, 1:].reshape(len(stacked_vector), 20, 20, 1) / 255, to_categorical(stacked_vector[:, 0])
 
     @staticmethod
-    def read_vectors(filename):
-        """
-        :param filename: path to .dat file containing samples
-        :return: raw numpy array containing samples
-        """
+    def _read_vectors(filename):
         return np.fromfile(filename, dtype=np.uint8).reshape(-1, 401)
 
 
 if __name__ == "__main__":
     dice = Dice(True)
     dice.benchmark_model('data/dice_09.dat')
-    image = Dice.load_data(Dice.read_vectors('data/dice_09.dat'))[0][0]
-    image2 = Dice.load_data(Dice.read_vectors('data/dice_09.dat'))[0][5]
+    image = Dice.load_data('data/dice_09.dat')[0][0]
+    image2 = Dice.load_data('data/dice_09.dat')[0][5]
     print(dice.predict(image))
     print(dice.predict(image2))
